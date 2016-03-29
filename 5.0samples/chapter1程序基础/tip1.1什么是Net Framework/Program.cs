378691d865718952f4662504e77f373d5a302b35
yhhno <516061849@qq.com>
2016-03-29 18:00:41 +0800

    tip 1.1 什么是net framework

---
+++ 5.0samples/chapter1程序基础/tip1.1什么是Net Framework/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tip1._1什么是Net_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+
+        /*********1.1 什么是.net framework*****
+         
+         微软官方说法：.Net Framework 是一个集成在Windows中的组件，它支持生成和运行下一代应用程序与XML Web Services. 大体上讲， 它是一种技术平台。
+         旨在实现以下目标：
+               》提供一个一致的面向对象的编程环境，而无论对象代码是在本地存储和执行，还是在本地执行但在Internet上发布，或者是在远程执行。
+               》提供一个将软件部署和版本控制冲突最小化的代码执行环境。
+               》提供一个可提高代码（包括由未知的或不完全受信任的第三方创建的代码）执行安全性的代码执行环境。
+               》提供一个可消除脚本环境或解释环境的性能问题的代码执行环境。
+               》使开发人员的经验在面对类型不大相同的应用程序（如基于windows的应用程序和基于web的应用程序）时保持一致
+               》按照工业标准生成所有通信，以确保基于。net framework的代码可与任何其他代码集成
+              
+         既然。Net 是一个环境，那么它包含什么呢？它包含两个方面的内容，一是公共语言运行时（common language runtime）；二是net framework类库。
+         公共语言运行时时。net framework的基础，一切net framework的程序都运行在以它为基础的平台上，公共语言运行时有点类似于Java中的虚拟机，它提供
+         内存管理、线程管理、和远程管理等核心服务，并且还强制实施严格的类型安全以及可提高安全性和可靠性的其他形式的代码准确性，这可能对初学者可能有点
+         难以理解，比如说在c++ 中，类型检查、内存越界、内存释放等需要程序员进行把握，以免出现错误，但是在net framework中，这些都由公共语言运行时来完成，
+         大大简化了编程的复杂性。
+         
+         net framework类库是一个综合性的面向对象的可重用类型集合，可以使用它开发多种应用程序。这就是面向对象语言的代码可重用性特点，在类库中包括比如通用基础类、集合类、线程及同步类、xml类等等
+         
+         net framework 类库可开发的程序种类有传统的命令行或图形用户界面（gui）应用程序，也包括基于asp.net所提供的最新创新的应用程序（web窗体和xml web services）。
+        
+        
+         
+         
+         
+         */
+
+
+    }
+}
